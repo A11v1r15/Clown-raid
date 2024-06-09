91c725a623281911a8ccf8cb6c94fb56827104d5
Álvaro Carvalho <a11v1r15@Rocketmail.com>
2024-06-08 23:17:30 -0300

    A11v1r15 patch 1 1 (#2)
    * Update SellerEntityModel.java
    
    * Add files via upload (#1)

--- src/client/java/net/a11v1r15/clownraid/SellerEntityModel.java
+++ src/client/java/net/a11v1r15/clownraid/SellerEntityModel.java
@@ -13,6 +13,11 @@ public class SellerEntityModel<T extends SellerEntity> extends VillagerResemblin
 
     public static TexturedModelData getTexturedModelData() {
         ModelData modelData = VillagerResemblingModel.getModelData();
+        ModelPartData root  = modelData.getRoot();
+        ModelPartData head  = root.getChild(EntityModelPartNames.HEAD);
+        ModelPartData hat = head.getChild(EntityModelPartNames.HAT);
+        ModelPartData hat_rim = hat.addChild(EntityModelPartNames.HAT_RIM, ModelPartBuilder.create().uv(38, 51).cuboid(-6.0F, -6.0F, -6.0F, 12.0F, 12.0F, 1.0F, new Dilation(0.1F)), ModelTransform.of(0.0F, 0.0F, 0.0F, -1.5708F, 0.0F, 0.0F));
+        
         return TexturedModelData.of(modelData, 64, 64);
     }
 }
