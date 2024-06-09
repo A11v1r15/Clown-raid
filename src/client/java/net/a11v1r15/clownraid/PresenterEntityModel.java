ccb8791eec20faa767ef8efc57457fe6b934d60c
Álvaro Carvalho <a11v1r15@Rocketmail.com>
2024-06-08 22:58:13 -0300

    :bento: PresenterEntityModel

--- src/client/java/net/a11v1r15/clownraid/PresenterEntityModel.java
+++ src/client/java/net/a11v1r15/clownraid/PresenterEntityModel.java
@@ -13,6 +13,11 @@ public class PresenterEntityModel<T extends PresenterEntity> extends VillagerRes
 
     public static TexturedModelData getTexturedModelData() {
         ModelData modelData = VillagerResemblingModel.getModelData();
+        ModelPartData root  = modelData.getRoot();
+        ModelPartData head  = root.getChild(EntityModelPartNames.HEAD);
+        ModelPartData hat = head.getChild(EntityModelPartNames.HAT);
+        ModelPartData hat_rim = hat.addChild(EntityModelPartNames.HAT_RIM,  ModelPartBuilder.create().uv(44, 53).cuboid(-6.0F, -6.0F, -6.0F, 12.0F, 12.0F, 1.0F, new Dilation(0.1F)), ModelTransform.of(0.0F, 0.0F, 0.0F, -1.5708F, 0.0F, 0.0F));
+    
         return TexturedModelData.of(modelData, 64, 64);
     }
 }
