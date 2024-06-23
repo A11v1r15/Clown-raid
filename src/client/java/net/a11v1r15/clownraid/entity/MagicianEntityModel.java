3d7d4b893e21a8414490fd039105089494d3d433
Álvaro Carvalho <a11v1r15@Rocketmail.com>
2024-06-23 19:07:19 -0300

    :bento: EntityModel

---
+++ src/client/java/net/a11v1r15/clownraid/entity/MagicianEntityModel.java
@@ -0,0 +1,23 @@
+package net.a11v1r15.clownraid.entity;
+
+import net.fabricmc.api.EnvType;
+import net.fabricmc.api.Environment;
+import net.minecraft.client.model.*;
+import net.minecraft.client.render.entity.model.*;
+
+@Environment(EnvType.CLIENT)
+public class MagicianEntityModel<T extends MagicianEntity> extends VillagerResemblingModel<T> {
+    public MagicianEntityModel(ModelPart modelPart) {
+        super(modelPart);
+    }
+
+    public static TexturedModelData getTexturedModelData() {
+        ModelData modelData = VillagerResemblingModel.getModelData();
+        ModelPartData root  = modelData.getRoot();
+        ModelPartData head  = root.getChild(EntityModelPartNames.HEAD);
+        ModelPartData hat = head.getChild(EntityModelPartNames.HAT);
+        ModelPartData hat_rim = hat.addChild(EntityModelPartNames.HAT_RIM, ModelPartBuilder.create().uv(38, 51).cuboid(-6.0F, -6.0F, -6.0F, 12.0F, 12.0F, 1.0F, new Dilation(0.1F)), ModelTransform.of(0.0F, 0.0F, 0.0F, -1.5708F, 0.0F, 0.0F));
+
+        return TexturedModelData.of(modelData, 64, 64);
+    }
+}
